715cf1181a1b196a8fe4a15c1fbc4142341f49f8
Tom Johnson <johnson.tom66@gmail.com>
2016-05-14 13:28:10 -0500

    Added console form
    First pass at console form GUI

--- SquadRCON/frmLogin.Designer.cs
+++ SquadRCON/frmLogin.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
@@ -40,21 +41,21 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(185, 24);
+            this.textBox1.Location = new System.Drawing.Point(194, 34);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(162, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(185, 51);
+            this.textBox2.Location = new System.Drawing.Point(194, 61);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(74, 20);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(185, 78);
+            this.textBox3.Location = new System.Drawing.Point(194, 88);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(162, 20);
             this.textBox3.TabIndex = 2;
@@ -62,7 +63,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(83, 27);
+            this.label1.Location = new System.Drawing.Point(94, 37);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(54, 13);
             this.label1.TabIndex = 3;
@@ -71,7 +72,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(83, 54);
+            this.label2.Location = new System.Drawing.Point(94, 64);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(29, 13);
             this.label2.TabIndex = 4;
@@ -80,7 +81,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(83, 81);
+            this.label3.Location = new System.Drawing.Point(94, 91);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(90, 13);
             this.label3.TabIndex = 5;
@@ -117,6 +118,7 @@
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmLogin";
             this.Text = "Squad RCON - Login";
             this.ResumeLayout(false);
---
+++ SquadRCON/frmMainConsole.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace SquadRCON
+{
+    partial class grpPlayers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(grpPlayers));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.grpConsole = new System.Windows.Forms.GroupBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.lvwPlayers = new System.Windows.Forms.ListView();
+            this.colSlot = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colPlayer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colSteamId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtCommand = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.grpConsole.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(177, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lvwPlayers);
+            this.groupBox1.Controls.Add(this.btnRefresh);
+            this.groupBox1.Location = new System.Drawing.Point(12, 98);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(510, 558);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Player List";
+            // 
+            // grpConsole
+            // 
+            this.grpConsole.Controls.Add(this.btnClear);
+            this.grpConsole.Controls.Add(this.btnSend);
+            this.grpConsole.Controls.Add(this.textBox1);
+            this.grpConsole.Controls.Add(this.txtCommand);
+            this.grpConsole.Location = new System.Drawing.Point(528, 98);
+            this.grpConsole.Name = "grpConsole";
+            this.grpConsole.Size = new System.Drawing.Size(492, 501);
+            this.grpConsole.TabIndex = 2;
+            this.grpConsole.TabStop = false;
+            this.grpConsole.Text = "Console Command";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(395, 507);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(109, 36);
+            this.btnRefresh.TabIndex = 7;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // lvwPlayers
+            // 
+            this.lvwPlayers.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colSlot,
+            this.colPlayer,
+            this.colSteamId});
+            this.lvwPlayers.GridLines = true;
+            this.lvwPlayers.Location = new System.Drawing.Point(7, 20);
+            this.lvwPlayers.Name = "lvwPlayers";
+            this.lvwPlayers.Size = new System.Drawing.Size(497, 481);
+            this.lvwPlayers.TabIndex = 8;
+            this.lvwPlayers.UseCompatibleStateImageBehavior = false;
+            this.lvwPlayers.View = System.Windows.Forms.View.Details;
+            // 
+            // colSlot
+            // 
+            this.colSlot.Text = "Slot";
+            // 
+            // colPlayer
+            // 
+            this.colPlayer.Text = "Player";
+            this.colPlayer.Width = 241;
+            // 
+            // colSteamId
+            // 
+            this.colSteamId.Text = "Steam64Id";
+            this.colSteamId.Width = 165;
+            // 
+            // txtCommand
+            // 
+            this.txtCommand.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtCommand.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.txtCommand.Location = new System.Drawing.Point(7, 20);
+            this.txtCommand.Name = "txtCommand";
+            this.txtCommand.Size = new System.Drawing.Size(479, 20);
+            this.txtCommand.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 125);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(479, 370);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(261, 46);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(109, 36);
+            this.btnSend.TabIndex = 8;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(376, 46);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(109, 36);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(911, 620);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(109, 36);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // grpPlayers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1032, 668);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.grpConsole);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "grpPlayers";
+            this.Text = "Squad RCON - Console";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.grpConsole.ResumeLayout(false);
+            this.grpConsole.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox grpConsole;
+        private System.Windows.Forms.ListView lvwPlayers;
+        private System.Windows.Forms.ColumnHeader colSlot;
+        private System.Windows.Forms.ColumnHeader colPlayer;
+        private System.Windows.Forms.ColumnHeader colSteamId;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtCommand;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
