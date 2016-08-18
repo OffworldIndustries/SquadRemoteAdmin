e1b20efb562c8a6475077681893cf5478567a8f1
Tom Johnson <johnson.tom66@gmail.com>
2016-08-18 17:33:53 -0500

    Added QueryMaster library back after adding support for unicode player names

---
+++ Squad.Admin.Console/RCON/ServerProxy.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+using QueryMaster;
+using QueryMaster.Steam;
+using QueryMaster.GameServer;
+using Squad.Admin.Console.Utilities;
+
+namespace Squad.Admin.Console.RCON
+{
+    public class ServerProxy
+    {
+        private Server squadServer = null;
+        private ServerConnectionInfo serverConnectionInfo = new ServerConnectionInfo();
+
+
+        public ServerProxy()
+        { }
+
+
+        public bool Connect(ServerConnectionInfo connectionInfo)
+        {
+            this.serverConnectionInfo = connectionInfo;
+            this.squadServer = ServerQuery.GetServerInstance(EngineType.Source, new IPEndPoint(this.serverConnectionInfo.ServerIP, this.serverConnectionInfo.ServerPort));
+            return this.squadServer.GetControl(this.serverConnectionInfo.Password);
+        }
+
+        public void Disconnect()
+        {
+            this.squadServer.Dispose();
+        }
+
+        #region COMMANDS
+
+        public string GetPlayerList()
+        {
+            if (this.squadServer.GetControl(this.serverConnectionInfo.Password))
+            {
+                return this.squadServer.Rcon.SendCommand("ListPlayers", true);
+            }
+            else
+            {
+                return "Unexpected error! Unable to communicate with the Squad Server!";
+            }
+        }
+
+        public string SendCommand(string command)
+        {
+            if (this.squadServer.GetControl(this.serverConnectionInfo.Password))
+            {
+                return this.squadServer.Rcon.SendCommand(command, false);
+            }
+            else
+            {
+                return "Unexpected error! Unable to communicate with the Squad Server!";
+            }
+        }
+
+        #endregion
+
+    }
+}
